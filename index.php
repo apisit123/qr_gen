c8995e823601f38b05b659aeda241bec27217038
A'ew Apisit <aew.apisit@gmail.com>
2019-05-06 04:31:42 +0700

    dfsdf

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
     require_once 'vendor/autoload.php';
-    echo "sdasd";
+
     if(isset($_GET['val']) && $_GET['val']!=""){
+
         $text_qrcode = urldecode(trim($_GET['val']));
+        echo $text_qrcode;
         header('Content-Type: image/png');
         \PHPQRCode\QRcode::png("CC".$text_qrcode, 'php://output', 'L', 4, 1);    
     }
