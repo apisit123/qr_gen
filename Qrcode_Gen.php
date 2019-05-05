afbe7d0650e3f16116917a1acb429db63815c737
A'ew Apisit <aew.apisit@gmail.com>
2019-05-06 04:25:57 +0700

    dfsdf

---
+++ Qrcode_Gen.php
@@ -0,0 +1,8 @@
+<?php
+    require_once 'vendor/autoload.php';
+    if(isset($_GET['val']) && $_GET['val']!=""){
+        $text_qrcode = urldecode(trim($_GET['val']));
+        header('Content-Type: image/png');
+        \PHPQRCode\QRcode::png("CC".$text_qrcode, 'php://output', 'L', 4, 1);    
+    }
+?>
